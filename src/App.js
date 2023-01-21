d102526e4c610825b5c64ea31e5fe3b5a99d480d
Andrea <andiserm@gmail.com>
2023-01-21 17:42:09 -0300

    Agrego solucion a tests fallidos

--- src/App.js
+++ src/App.js
@@ -14,16 +14,38 @@ class App extends Component {
       newTask: ''
     }
   }
+
+  handleChangeNewTask = (e) => {
+    this.setState({ newTask: e.target.value })
+  }
+
+  handleAddTask = (e) => {
+    e.preventDefault()
+    if (this.state.newTask) {
+      const newTask = { id: this.state.tasks.length + 1, name: this.state.newTask }
+      this.setState({ tasks: this.state.tasks.concat(newTask), newTask: '' })
+    } else {
+      this.setState({ newTask: '' })
+    }
+  }
+
+  handleDoneTask = (id) => {
+    const doneTask = this.state.tasks.find(item => item.id === id)
+    const tasks = this.state.tasks.filter(item => item.id !== id)
+    const newTasks = [...tasks, {...doneTask, done: !doneTask.done }].sort((a, b) => a.id - b.id)
+    this.setState({ tasks: newTasks })
+  }
+
   render() {
     return (
       <div className="wrapper">
         <div className="list">
           <h3>Por hacer:</h3>
           <ul className="todo">
-            {this.state.tasks.map((task, index) => <li key={task.id}>{task.name}</li>)}
+            {this.state.tasks.map((task, index) => <li className={task.done ? 'done' : ''} onClick={() => this.handleDoneTask(task.id)} key={task.id}>{task.name}</li>)}
           </ul>
-          <form>
-            <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" value={this.state.newTask} />
+          <form onSubmit={this.handleAddTask.bind(this)}>
+            <input className={this.state.newTask.length === 0 ? 'error' : ''} onChange={this.handleChangeNewTask.bind(this)} type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" value={this.state.newTask} />
           </form>
         </div>
       </div>
